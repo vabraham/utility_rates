d2e1406f7f942f0b0c06a348ee9cd6e9627baf88
Vineet Abraham <vabraham24@gmail.com>
2015-02-20 00:11:17 -0800

    added public rate api file

---
+++ rate_api_public.R
@@ -0,0 +1,64 @@
+## Please note that you must replace the keys below ("sample_key" and "sample_key1") with your own keys for this script to work
+## You can get your own keys directly from https://developers.google.com/maps/signup and http://www.data.gov/developers/apis
+rm(list=ls())
+install.packages("httr")
+library(httr)
+
+# Google maps Javascript AP1 v3
+sample1 <- GET("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=sample_key")
+content(sample1)
+# Or
+add1 <- "1600 Amphitheatre Parkway, Mountain View, CA"
+key <- "sample_key"
+sample1 <- GET("https://maps.googleapis.com/maps/api/geocode/json", query = list(address = add1, key = key))
+content(sample1)
+
+# Data.gov Utility Rates API
+sample2 <- GET("http://api.data.gov/nrel/utility_rates/v3.json?api_key=sample_key1&lat=37.778462&lon=-122.411593")
+content(sample2)
+# Or
+key <- "sample_key1"
+lat <- 37.778462
+lon <- -122.411593
+sample2 = GET("http://api.data.gov/nrel/utility_rates/v3.json", query = list(api_key = key, lat = lat, lon = lon))
+content(sample2)
+
+# Combining both the Google Maps API and the Data.gov API to get utility rates by city
+google_url <- "https://maps.googleapis.com/maps/api/geocode/json"
+gov_url <- "http://api.data.gov/nrel/utility_rates/v3.json"
+geoCode <- function(address,verbose=FALSE) {
+  r <- GET(google_url, query = list(address = address, key = "sample_key"))
+  stop_for_status(r)
+  result1 <- content(r)
+  
+  if (!identical(result1$status, "OK")) {
+    warning("Please input a valid US address.", call. = FALSE)
+    return(c(NA,NA,NA,NA,NA,NA))
+  }
+  
+  s <- GET(gov_url, query = list(api_key = "sample_key1", lat = result1$results[[1]]$geometry$location$lat, lon = result1$results[[1]]$geometry$location$lng))
+  stop_for_status(s)
+  result2 <- content(s)
+  
+  if (result2$outputs$utility_name == "no data") {
+    warning("Please input a valid US address.", call. = FALSE)
+    return(c(NA,NA,NA,NA,NA,NA))
+  }
+  
+  first <- result1$results[[1]]
+  second <- result2$outputs
+  list(
+    lat = first$geometry$location$lat,
+    lon = first$geometry$location$lng,
+    type = first$geometry$location_type,
+    address = first$formatted_address,
+    utility = second$utility_info[[1]]$utility_name,
+    residential_rate_kwh = second$residential
+  )
+}
+
+# Test with different addresses
+geoCode("San Francisco, CA")
+geoCode("Beijing, China")
+geoCode("New York, NY")
+geoCode("the planet uranus")
