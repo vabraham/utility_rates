8e8900d02c362791bee56eb1b1e8aee561c9ae64
Vineet Abraham <vabraham24@gmail.com>
2015-02-20 19:30:53 -0800

    minor updates

--- rate_api_public.R
+++ rate_api_public.R
@@ -1,33 +1,35 @@
-## Please note that you must replace the keys below ("sample_key" and "sample_key1") with your own keys for this script to work
-## You can get your own keys directly from https://developers.google.com/maps/signup and http://www.data.gov/developers/apis
+## Please note that you must replace the key below ("sample_key")with your own key for this script to work
+## You can get your own key directly from http://www.data.gov/developers/apis
 rm(list=ls())
 install.packages("httr")
 library(httr)
 
 # Google maps Javascript AP1 v3
-sample1 <- GET("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=sample_key")
+sample1 <- GET("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA")
 content(sample1)
 # Or
 add1 <- "1600 Amphitheatre Parkway, Mountain View, CA"
-key <- "sample_key"
-sample1 <- GET("https://maps.googleapis.com/maps/api/geocode/json", query = list(address = add1, key = key))
-content(sample1)
+sample1 <- GET("https://maps.googleapis.com/maps/api/geocode/json", query = list(address = add1))
+result1 <- content(sample1)
+result1$results[[1]]$geometry$location$lat
+result1$results[[1]]$geometry$location$lng
 
 # Data.gov Utility Rates API
-sample2 <- GET("http://api.data.gov/nrel/utility_rates/v3.json?api_key=sample_key1&lat=37.778462&lon=-122.411593")
+sample2 <- GET("http://api.data.gov/nrel/utility_rates/v3.json?api_key=sample_key&address=1600+Amphitheatre+Parkway,+Mountain+View,+CA")
 content(sample2)
 # Or
-key <- "sample_key1"
-lat <- 37.778462
-lon <- -122.411593
-sample2 <- GET("http://api.data.gov/nrel/utility_rates/v3.json", query = list(api_key = key, lat = lat, lon = lon))
-content(sample2)
+key <- "sample_key"
+add1 <- "1600 Amphitheatre Parkway, Mountain View, CA"
+sample2 = GET("http://api.data.gov/nrel/utility_rates/v3.json", query = list(api_key = key, address = add1))
+result2 <- content(sample2)
+result2$outputs$utility_info[[1]]$utility_name
+result2$outputs$residential
 
-# Combining both the Google Maps API and the Data.gov API to get utility rates by city
+# Combining both the Google Maps API and the Data.gov API to get utility rates by city - preferred
 google_url <- "https://maps.googleapis.com/maps/api/geocode/json"
 gov_url <- "http://api.data.gov/nrel/utility_rates/v3.json"
 geoCode <- function(address,verbose=FALSE) {
-  r <- GET(google_url, query = list(address = address, key = "sample_key"))
+  r <- GET(google_url, query = list(address = address))
   stop_for_status(r)
   result1 <- content(r)
   
@@ -36,7 +38,7 @@ geoCode <- function(address,verbose=FALSE) {
     return(c(NA,NA,NA,NA,NA,NA))
   }
   
-  s <- GET(gov_url, query = list(api_key = "sample_key1", lat = result1$results[[1]]$geometry$location$lat, lon = result1$results[[1]]$geometry$location$lng))
+  s <- GET(gov_url, query = list(api_key = "sample_key", lat = result1$results[[1]]$geometry$location$lat, lon = result1$results[[1]]$geometry$location$lng))
   stop_for_status(s)
   result2 <- content(s)
   
@@ -60,5 +62,3 @@ geoCode <- function(address,verbose=FALSE) {
 # Test with different addresses
 geoCode("San Francisco, CA")
 geoCode("Beijing, China")
-geoCode("New York, NY")
-geoCode("the planet uranus")
